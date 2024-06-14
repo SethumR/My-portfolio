cddef22d1d3239822d21406c651261b519252990
Sethum <sethumgelaka6@gmail.com>
2024-06-14 22:01:46 +0530

    Updated the nav bar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className="App">
+        <nav className="navbar">
+          <ul>
+            <li><a href="#home">HOME</a></li>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#skills">SKILLS</a></li>
+            <li><a href="#portfolio">PORTFOLIO</a></li>
+            <li><a href="#blog">BLOG</a></li>
+            <li><a href="#contact">CONTACT</a></li>
+          </ul>
+        </nav>
+    </div> 
+  )
+}
+
+export default Navbar
