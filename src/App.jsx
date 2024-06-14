1e83d029185de405a8591a73a28ee75ce9a1ad52
Sethum <sethumgelaka6@gmail.com>
2024-06-14 22:06:05 +0530

    Updated App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar/>
+    <Home/>
     </>
   )
 }
