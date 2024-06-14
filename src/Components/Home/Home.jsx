9b511b6c0482a8f895bb59a6e7b93a91d83c0c26
Sethum <sethumgelaka6@gmail.com>
2024-06-14 22:04:41 +0530

    Updated the home page

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <header className="header">
+        <h1>Sethum Ruberu</h1>
+        <p>Undergraduate | Developper | Blogger</p>
+        <button className="resume-button">My Resume</button>
+        <h2 className='text-4xl'>Hii evryone</h2>
+    </header>
+  )
+}
+
+export default Home
