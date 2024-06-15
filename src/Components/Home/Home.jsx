924a253799cd411ecb4e6c7bc51f7a6525a7a1ee
Sethum <sethumgelaka6@gmail.com>
2024-06-15 22:56:03 +0530

    Updated

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css'
 function Home() {
   return (
     <header className="header">
-        <h1 className='text-7xl font-semibold'>Nazhim Kalam</h1>
+        <h1 className='text-7xl font-semibold'>Sethum Ruberu</h1>
         <p className='text-2xl'>Undergraduate | Developper | Blogger</p>
         <button className="resume-button">My Resume</button>
     </header>
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -4,7 +4,17 @@ import './Skills.css'
 function Skills() {
   return (
     <div className='skills'>
-        <h1 className='text-6xl font-normal'>Skills</h1>
+        <h1 className='text-6xl font-normal '>Skills</h1>
+        <p>As a undergraduate Student I Work with Varitey of Languages and Frameworks </p>
+
+        <div>
+            <ul className="flex space-x-52">
+            <li>FRONTEND</li>
+            <li>BACKEND</li>
+            <li>DATABASE</li>
+            <li>GRAPHIC EDITING</li>
+            </ul>
+        </div>
     </div>
   )
 }
