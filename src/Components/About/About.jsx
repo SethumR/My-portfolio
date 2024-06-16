d2a518134cbc83313d870d63183e40f3a7407c31
Sethum <sethumgelaka6@gmail.com>
2024-06-16 13:20:22 +0530

    Updated

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 function About() {
   return (
     <section className="bg-gray-900 text-white py-20 px-10">
-      <div className='pl-24'>
+      <div className='pl-24 '>
         <h1 className="text-6xl font-semibold mb-10">About</h1>
-        <p className="text-xl mb-6 leading-9 tracking-wide	font-light	text-slate-200		">
+        <p className="text-xl mb-6 leading-9 tracking-wide	font-light	text-slate-200	max-w-7xl">
         I am Sethum Ruberu, a passionate computer science undergraduate at the Informatics Institute of Technology in Sri Lanka, proudly affiliated with the University of Westminster in the United Kingdom. My academic journey has been an exhilarating adventure, immersing me in the dynamic realms of software development, intricate algorithms, and insightful data analysis.
         </p>
-        <p className="text-xl leading-9	 mb-6 tracking-wide	font-light	text-slate-200	">
+        <p className="text-xl leading-9	 mb-6 tracking-wide	font-light	text-slate-200	max-w-7xl">
         Driven by an unwavering passion for technology and innovation, I am determined to leverage my skills to tackle real-world challenges and push the boundaries of computer science.
         </p>
-        <p className="text-xl leading-9	 mb-6 tracking-wide font-light	text-slate-200		">
+        <p className="text-xl leading-9	 mb-6 tracking-wide font-light	text-slate-200	max-w-7xl">
         Thriving in collaborative environments, I eagerly embrace the latest technological advancements, always seeking opportunities to learn and innovate.
         </p>
-        <p className="text-xl leading-9	 mb-6 tracking-wide	font-light	text-slate-200	">
+        <p className="text-xl leading-9	 mb-6 tracking-wide	font-light	text-slate-200	max-w-7xl">
         My ultimate vision is to leave a lasting impact on the tech industry through relentless curiosity and creative problem-solving. With a commitment to continuous growth and a keen eye for emerging trends, I aspire to contribute meaningfully to shaping the future of technology.
         </p>
       </div>
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,54 +1,52 @@
 import React from 'react';
 
-const Skills = () => {
+function Skills() {
   return (
-    <section className="text-gray-300 bg-gray-900 py-16">
-      <div className="container mx-auto px-8 md:px-16">
-        <h2 className="text-4xl font-bold mb-4">Skills</h2>
-        <p className="text-lg mb-12">As a developer, I've worked with a variety of technologies to create and manage my projects.</p>
-        <h3 className="text-3xl font-semibold text-purple-400 mb-8">Technical Skills</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+    <section className="text-gray-300 py-16">
+      <div className="container  px-8 md:px-16">
+        <h2 className="text-6xl font-semibold mb-10">Skills</h2>
+        <p className="mb-12 text-2xl font-medium text-slate-400	">As a developer, I've worked with a variety of Languages and Frameworks.</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5">
           <div>
-            <h4 className="text-xl font-semibold mb-4">Frontend Web</h4>
+            <h4 className="text-xl font-semibold mb-5 ">Frontend</h4>
             <ul>
-              <li className="mb-2"><span className="mr-2">🌐</span>React</li>
-              <li className="mb-2"><span className="mr-2">🌐</span>Next</li>
-              <li className="mb-2"><span className="mr-2">🌐</span>Angular</li>
-              <li className="mb-2"><span className="mr-2">🌐</span>JavaScript</li>
-              <li className="mb-2"><span className="mr-2">🌐</span>HTML</li>
+              <li className="mb-4"><span className="mr-2">🌐</span>HTML</li>
+              <li className="mb-4"><span className="mr-2">🌐</span>CSS</li>
+              <li className="mb-4"><span className="mr-2">🌐</span>JavaScript</li>
+              <li className="mb-4"><span className="mr-2">🌐</span>React</li>
+              <li className="mb-4"><span className="mr-2">🌐</span>Bootsrap</li>
+              <li className="mb-4"><span className="mr-2">🌐</span>Tailwind CSS</li>
             </ul>
           </div>
           <div>
-            <h4 className="text-xl font-semibold mb-4">Frontend Mobile</h4>
+            <h4 className="text-xl font-semibold mb-5">Backend</h4>
             <ul>
-              <li className="mb-2"><span className="mr-2">📱</span>React Native</li>
-              <li className="mb-2"><span className="mr-2">📱</span>Flutter</li>
+              <li className="mb-4"><span className="mr-2">📱</span>Node js</li>
+              <li className="mb-4"><span className="mr-2">📱</span>Express</li>
+              <li className="mb-4"><span className="mr-2">📱</span>Spring Boot</li>
             </ul>
           </div>
           <div>
-            <h4 className="text-xl font-semibold mb-4">Backend</h4>
+            <h4 className="text-xl font-semibold mb-5">Database</h4>
             <ul>
-              <li className="mb-2"><span className="mr-2">🛠️</span>Nest js</li>
-              <li className="mb-2"><span className="mr-2">🛠️</span>Node js</li>
-              <li className="mb-2"><span className="mr-2">🛠️</span>Express</li>
-              <li className="mb-2"><span className="mr-2">🛠️</span>Spring Boot</li>
-              <li className="mb-2"><span className="mr-2">🛠️</span>.NET</li>
+              <li className="mb-4"><span className="mr-2">🛠️</span>My SQL</li>
+              <li className="mb-4"><span className="mr-2">🛠️</span>Firebase Firestore</li>
+              <li className="mb-4"><span className="mr-2">🛠️</span>MongoDB</li>
             </ul>
           </div>
           <div>
-            <h4 className="text-xl font-semibold mb-4">Database</h4>
+            <h4 className="text-xl font-semibold mb-5">Graphic Editing</h4>
             <ul>
-              <li className="mb-2"><span className="mr-2">💾</span>MongoDB</li>
-              <li className="mb-2"><span className="mr-2">💾</span>Firebase Firestore</li>
-              <li className="mb-2"><span className="mr-2">💾</span>SQL</li>
-              <li className="mb-2"><span className="mr-2">💾</span>SQLite</li>
-              <li className="mb-2"><span className="mr-2">💾</span>AWS DynamoDB</li>
+              <li className="mb-4"><span className="mr-2">💾</span>Photoshop</li>
+              <li className="mb-4"><span className="mr-2">💾</span>Figma</li>
+              <li className="mb-4"><span className="mr-2">💾</span>Filmora</li>
+              <li className="mb-4"><span className="mr-2">💾</span>Premiere pro</li>
             </ul>
           </div>
         </div>
       </div>
     </section>
   );
-};
+}
 
 export default Skills;
