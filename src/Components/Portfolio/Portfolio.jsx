0006c99f029b8fb4e58cb81a66b3e3bcbfa6912c
Sethum <sethumgelaka6@gmail.com>
2024-06-17 22:40:36 +0530

    Portfolio updated

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,31 +1,29 @@
 import React from 'react'
-
 function Portfolio() {
   return (
     <div className='pl-32'>
         <h2 className="text-6xl font-semibold mb-10  tracking-wide">Portfolio</h2>
-        <p className="mb-12 text-2xl font-medium text-gray-300">As a developer, These are some of the projects I've worked on recently.</p>
+        <p className="mb-16 text-2xl font-medium text-gray-300 ">As a developer, These are some of the projects I've worked on recently.</p>
 
        
-          <div className="flex space-x-4">
-            {/* First Column */}
-            <div className="bg-gray-900 p-4 rounded-lg flex flex-col items-center">
+          <div className="flex space-x-10 mb-36 pl-8 ">
+            <div className="bg-slate-700 p-4 rounded-3xl flex flex-col items-center h-80 w-96 ">
               <img src="path/to/first-image.jpg" alt="First" className="mb-4 rounded-lg shadow-lg" />
-              <p className="text-white">Description for the first image</p>
-            </div>
+              <p className="text-white ">Description for the first image</p>
+             </div>
 
-            {/* Second Column */}
-            <div className="bg-gray-900 p-4 rounded-lg flex flex-col items-center">
-              <img src="path/to/second-image.jpg" alt="Second" className="mb-4 rounded-lg shadow-lg" />
-              <p className="text-white">Description for the second image</p>
-            </div>
+             <div className="bg-slate-700 p-4 rounded-3xl flex flex-col items-center h-80 w-96">
+              <img src="path/to/first-image.jpg" alt="First" className="mb-4 rounded-lg shadow-lg" />
+              <p className="text-white ">Description for the first image</p>
+             </div>
 
-            {/* Third Column */}
-            <div className="bg-gray-900 p-4 rounded-lg flex flex-col items-center">
-              <img src="path/to/third-image.jpg" alt="Third" className="mb-4 rounded-lg shadow-lg " />
-              <p className="text-white mb-32">Description for the third image</p>
-            </div>
+             <div className="bg-slate-700 p-4 rounded-3xl flex flex-col items-center h-80 w-96">
+              <img src="path/to/first-image.jpg" alt="First" className="mb-4 rounded-lg shadow-lg" />
+              <p className="text-white ">Description for the first image</p>
+             </div>
           </div>
+
+          
     </div>
   )
 }
