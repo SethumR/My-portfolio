d8aabfeb6a21111e4294a878f12692dd22c71f98
Sethum <sethumgelaka6@gmail.com>
2024-06-17 22:42:54 +0530

    pdated contact page

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='pl-32'>
+        <div>
+            <h2 className='text-6xl font-semibold mb-10  tracking-wide'>Contact</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
