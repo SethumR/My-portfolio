86df3548fb94106ab70c11687995235c12ef2536
Sethumrubeu <sethumgelaka6@gmail.com>
2024-09-09 16:41:06 +0530

    Updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='px-32 text-gray-300'>
         <hr className='mb-8'/>
-        <h2 className='text-center mb-12 leading-8	font-medium '>© 2024 Sethum Ruberu</h2>
+        <h2 className='text-center mb-12 leading-8	font-medium '> © 2024 Sethum Ruberu</h2>
     </div>
   )
 }
