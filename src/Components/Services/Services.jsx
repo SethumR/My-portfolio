c78bc5d19ce0b2db764b96ce4cdb401900de4dd2
Sethum Ruberu <160596225+SethumR@users.noreply.github.com>
2025-02-01 18:21:53 +0530

    Changed AOS animations

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -37,7 +37,7 @@ const Services = () => {
           Our Services
         </h2>
 
-        <div data-aos="flip-up">
+        <div data-aos="fade">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {services.map((service, index) => (
             <ServiceCard key={index} {...service} />
