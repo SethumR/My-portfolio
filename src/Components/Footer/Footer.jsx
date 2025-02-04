1a5676cb5265747639730993442a172955f6eae7
Sethum Ruberu <160596225+SethumR@users.noreply.github.com>
2025-02-04 19:06:44 +0530

    Added socail media icons to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React from "react";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { SiBuymeacoffee } from "react-icons/si";
 
 function Footer() {
   return (
     <footer className="px-4 sm:px-6 md:px-8 lg:px-16 xl:px-32 py-6 text-gray-300 mb-12">
-      <div className="container mx-auto">
-        <hr className=" mb-6" />
-        <p className="text-center text-sm sm:text-base font-medium">
+      <div className="container mx-auto text-center">
+        <hr className="mb-6 border-gray-600" />
+        <p className="text-sm sm:text-base font-medium">
           © {new Date().getFullYear()} Sethum Ruberu
         </p>
+        <div className="flex justify-center space-x-6 mt-6">
+          <a
+            href="https://github.com/SethumR"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub className="text-slate-300 text-[24px] sm:text-2xl hover:scale-110 transition-transform" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sethum-ruberu-90a369293/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin className="text-slate-300 text-[24px]  sm:text-2xl hover:scale-110 transition-transform" />
+          </a>
+          <a
+            href="https://www.instagram.com/sethumm.r/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="text-slate-300 text-[24px]  sm:text-2xl hover:scale-110 transition-transform" />
+          </a>
+          <a
+            href="https://buymeacoffee.com/sethumruberu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiBuymeacoffee className="text-slate-300 text-[24px]  sm:text-2xl hover:scale-110 transition-transform" />
+          </a>
+        </div>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
-
+export default Footer;
